ef45b516d34cce92fa82e8a4923f86ee8fe78052
reizamahmud12 <110676256+reizamahmud12@users.noreply.github.com>
2022-08-05 20:37:58 +0700

    Add files via upload

---
+++ kasir.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<?php 
+session_start();
+include "koneksi/koneksi.php";
+include "koneksi/function.php";
+if (!isset($_SESSION['username_user'])) {
+    header('location:index.php');
+} 
+// elseif ($_SESSION['akses'] != "Kasir")
+// {
+//     echo "<script>alert('Maaf Anda tidak berhak mengakses halaman ini')</script>";
+//     echo "<script>window.history.back();</script>";
+// }
+?>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="assets/js/sweetalert.js"></script>
+
+    <title>Rumah makan reiza</title>
+</head>
+
+<body>
+    <?php 
+                if(!isset($_GET['halaman'])) {
+                        error_reporting(0);
+                    }
+                    // if ($_GET['halaman'] == 'dashboard') {
+                    //     include "dashboard/dashboard_admin.php";
+                    // }
+                    // Tutup Dashboard
+
+                    // Parsing halaman Pegawai
+                    if ($_GET['halaman'] == 'kasir') {
+                        include "system/transaksi/kasir/tampil.php";
+                    }
+                ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.mask.js"></script>
+
+
+</body>
+
+</html>
